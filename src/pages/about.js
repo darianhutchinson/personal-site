1a64f54e6afafb4fa5078e08353cec1bbac53c57
Darian Hutchinson <dhutch2@pdx.edu>
2021-11-30 12:02:52 -0800

    Added work on the contact form

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,24 +11,27 @@ const About = () =>{
           <div className="container">
             <h3>Darian Hutchinson</h3>
             <p className="job-title">Software Engineer</p>
-            <p>Hello There! My name is Darian Hutchinson and I am a full time database administrator and part time web developer. 
-              Our online world is all about data and I love to organize, secure and visualize that data. The speed at which we can 
-              process information nowadays can lead to an overwhelming amount of information. My specialty is to break down that 
-              information into easily accessible and understandable chunks that are workable to those who need it. In many cases, 
-              my passion for data collides with website design! These situations are where I find the most joy in my work.<br></br>
+            <div className="content">
+              <p>Hello There! My name is Darian Hutchinson and I am a full time database administrator and part time web developer. 
+                Our online world is all about data and I love to organize, secure and visualize that data. The speed at which we can 
+                process information nowadays can lead to an overwhelming amount of information. My specialty is to break down that 
+                information into easily accessible and understandable chunks that are workable to those who need it. In many cases, 
+                my passion for data collides with website design! These situations are where I find the most joy in my work.<br></br>
+                
+                <br></br>I graduated with a Bachelor's degree in Computer Science from Portland State University in 2022. Before that, I 
+                obtained a Bachelor's degree in Music also from PSU. Having both an arts and science background I have found that web 
+                development is the perfect combination of the two. If you are looking to design/develop a website that can display and use your 
+                hard-earned data, feel free to reach out to me!
+              </p>
+            </div>
               
-              <br></br>I graduated with a Bachelor's degree in Computer Science from Portland State University in 2022. Before that, I 
-              obtained a Bachelor's degree in Music also from PSU. Having both an arts and science background I have found that web 
-              development is the perfect combination of the two. If you are looking to design/develop a website that can display and use your 
-              hard-earned data, feel free to reach out to me!
-            </p>
             <a href="/projects" class="button">Projects</a>
-          </div>
-          <div className="empty-div">
           </div>
         </div>
         
       </body> 
   );
+
+  
 }
 export default About;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import './contact.css';
 const Contact = () =>{
   return (
-    <div>
-      <h3>Contact</h3>
-      <div>
-          <p>This is my Contact page!</p>
-      </div>
-    </div>
+    <body>
+      <div className="heading">
+        <p className="contact-p">Feel free to ask me any questions!</p>
+      </div> 
+      <form className="">
+        <div class="form-group">
+          <label for="fName">First Name</label>
+          <input type="text" className="form-control" id="fName" placeholder="Your first name here..."></input>
+        </div>
+        <div class="form-group">
+          <label for="lName">Last Name</label>
+          <input type="text" className="form-control" id="lName" placeholder="Your last name here..."></input>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" className="form-control" id="email" placeholder="Your email here..."></input>
+        </div>
+        <div class="form-group">
+          <textarea className="form-control">Send me a message!</textarea>
+        </div>
+        <div class="form-group btn-container">
+          <input type="submit" value="Send it!"></input>
+        </div>
+      </form>
+    </body>
   );
 }
 export default Contact;
