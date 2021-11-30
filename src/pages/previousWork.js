2f58c789fa5ee0c3f27a2b5f8664d461bbb9a141
Darian Hutchinson <dhutch2@pdx.edu>
2021-11-30 13:51:54 -0800

    Added to icons to previous work page

--- src/pages/previousWork.js
+++ src/pages/previousWork.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import './previousWork.css';
+import { FaMusic } from 'react-icons/fa'
+import { GrPersonalComputer } from 'react-icons/gr'
+import { AiFillFileExcel } from 'react-icons/ai'
+import { ImDatabase } from 'react-icons/im'
 const PreviousWork = () =>{
+  function musicPrompt(){
+    alert("You triggered music prompt")
+  }
   return (
-    <div>
+    <body>
       <h3>Previous Work</h3>
       <div>
-          <p>This is my Previous Work page!</p>
+          <p>Here is some sutff I have accomplished so far!</p>
+          <p>Click on the icons for more info!</p>
       </div>
-    </div>
+      <div className="grid-container">
+        <div className="grid-item">
+          <FaMusic className="icon" onClick={musicPrompt}></FaMusic>
+          <p>Bachelor's of Music Degree</p>
+        </div>
+        <div className="grid-item">
+          <GrPersonalComputer className="icon"></GrPersonalComputer>
+          <p>Bachelor's of Computer Science Degree</p>
+        </div>
+        <div className="grid-item">
+          <AiFillFileExcel className="icon"></AiFillFileExcel>
+          <p>Head of Product Management</p>
+        </div>
+        <div className="grid-item">
+          <ImDatabase className="icon"></ImDatabase>
+          <p>Database Admin Internship</p>
+        </div>
+      </div>
+    </body>
   );
 }
 export default PreviousWork;
